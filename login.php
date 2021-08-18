ef7b65505a79a75280905a02f53a76437ea24a45
luaS <coy.saul@gmail.com>
2021-08-18 15:08:31 -0600

    login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/stlogin.css">
+  <title>Venta de Autos LUAS</title>
+</head>
+<body>
+  <div class = "contenedor">
+    <img src="img/logo.jpg" alt="">
+    <h1>Ingrese Aquí</h1>
+    <form action="">
+      <!--usuario-->
+      <label for="usuario">Usuario</label>
+      <input type="text" placeholder= "Ingrese Usuario">
+      <!--contraseña-->
+      <label for="contra">Contraseña</label>
+      <input type="text" placeholder= "Ingrese Contraseña">
+
+      <input type="submit" value= "Iniciar Sesión">
+
+    </form>
+
+  </div>
+</body>
+</html>
