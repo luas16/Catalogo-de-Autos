2d3df9987bc7d3fb9daf4d06f8903286e198075c
luaS <coy.saul@gmail.com>
2021-08-17 23:51:42 -0600

    formularioInsertar

---
+++ alertNeg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/modal.css">
+</head>
+<body>
+	<div id = modal1 class="fondo">
+		<div class="contenedor">
+			<h2 class="mensaje">No se adjunto ninguna foto</h2>
+			<a class="btnAceptar" href="formVehiculo.php">Aceptar</a>
+		</div>
+	</div>
+
+	<script type="text/javascript">
+		document.getElementById('modal1').style.display = 'flex';
+	</script>
+</body>
+</html>
--- insertarVehiculo.php
+++ insertarVehiculo.php
@@ -1,28 +1,31 @@
 <?php
 include 'php/conexion.php  ';
 
+
+//recogemos toda la informacion y la enviamos a nuestra base de datos media vez ingresen fotos
+$marca = $_POST['marca'];
+$linea = $_POST['linea'];
+$tipo = $_POST['tipo'];
+$transmision = $_POST['transmision'];
+$modelo = $_POST['modelo'];
+$km = $_POST['km'];
+$traccion = $_POST['traccion'];
+$combustible = $_POST['combustible'];
+$color = $_POST['color'];
+$precio = $_POST['precio'];
+$cantidad_puertas = $_POST['cantidad_puertas'];
+//se envia la informacion a traves de esta consulta
+$squery = "INSERT INTO vehiculos(correlativo, marca, linea, tipo, transmision, modelo, km, traccion, combustible, color, precio, cantidad_puertas) VALUES (NULL, '$marca', '$linea', '$tipo', '$transmision', '$modelo', '$km', '$traccion', '$combustible', '$color', '$precio', '$cantidad_puertas')";
+
+mysqli_query($conexion, $squery);
+
+//se recoge al id del ultimo dato ingresado en nuestra BD
+$idvehiculo = mysqli_insert_id($conexion);
+
+
 foreach ($_FILES["archivo"]["tmp_name"] as $key => $tmp_name) {
 
 	if ($_FILES["archivo"]["name"][$key]) {
-		//recogemos toda la informacion y la enviamos a nuestra base de datos media vez ingresen fotos
-		$marca = $_POST['marca'];
-		$linea = $_POST['linea'];
-		$tipo = $_POST['tipo'];
-		$transmision = $_POST['transmision'];
-		$modelo = $_POST['modelo'];
-		$km = $_POST['km'];
-		$traccion = $_POST['traccion'];
-		$combustible = $_POST['combustible'];
-		$color = $_POST['color'];
-		$precio = $_POST['precio'];
-		$cantidad_puertas = $_POST['cantidad_puertas'];
-		//se envia la informacion a traves de esta consulta
-		$squery = "INSERT INTO vehiculos(correlativo, marca, linea, tipo, transmision, modelo, km, traccion, combustible, color, precio, cantidad_puertas) VALUES (NULL, '$marca', '$linea', '$tipo', '$transmision', '$modelo', '$km', '$traccion', '$combustible', '$color', '$precio', '$cantidad_puertas')";
-
-		mysqli_query($conexion, $squery);
-		//se recoge al id del ultimo dato ingresado en nuestra BD
-		$idvehiculo = mysqli_insert_id($conexion);
-
 		//Como se ingresaron las fotos se realiza lo siguiente para recopilar la informacion necesaria.
 		$nombreArchivo = $_FILES["archivo"]["name"][$key];//recopilamos nombre de archivo
 		$ubicacion = $_FILES["archivo"]["tmp_name"][$key];//recopilamos la ubicacion en el servidor
@@ -42,20 +45,21 @@ foreach ($_FILES["archivo"]["tmp_name"] as $key => $tmp_name) {
 		//validando y moviendo imagenes
 		if(move_uploaded_file($ubicacion, $ubicacionSalida))
 		{
-			echo "Ha sido trasladada con exito";
 			$queryf = "INSERT INTO fotos_autos(correlativo, id_vehiculo, ubicacion) Values (NULL, '$idvehiculo' ,'$ubicacionSalida')";
 			mysqli_query($conexion, $queryf);
 
 		}else
 		{
-			echo "Error, realice de nuevo el proceso";
+			echo "Error, No ha subido ninguna foto";
 		}
 
+		
 		closedir($dir);//cerrando carpeta
+		header('location: alertPos.php');
 	}
 	else
 	{
-		header('location: alertPos.php');
+		header('location: alertNeg.php');
 	}
 
 }
